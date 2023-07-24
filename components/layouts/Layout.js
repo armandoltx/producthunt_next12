8736a78425e24295f8ad60c2da7553bee49718b6
Armando Ruiz <armando.ruiz@industrie.co>
2023-07-24 15:29:21 +1000

    608. Agregando normalize, fonts e imagenes al layout principal

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import { Global, css } from '@emotion/react'
+import Head from "next/head";
 
 const Layout = props => {
   return(
@@ -52,7 +53,15 @@ const Layout = props => {
           }
       `}
     />
+      <Head>
+        <title>Product Hunt Firebase y Next.js</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet" />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+
       <Header />
+
       <main>
         {props.children}
       </main>
