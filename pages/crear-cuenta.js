f79f901ec59f54a6a24888be6d6d2a65de76d8c1
Armando Ruiz <armando.ruiz@industrie.co>
2023-08-02 14:58:35 +1000

    619. Validando el Formulario de crear cuenta y mostrando los mensajes de error

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layouts/Layout'
 import {css} from '@emotion/react';
-import {Formulario, Campo, InputSubmit} from '../components/ui/Formulario';
+import {Formulario, Campo, InputSubmit, Error} from '../components/ui/Formulario';
 
 // validaciones
 import useValidacion from '../hooks/useValidacion';
@@ -13,7 +13,7 @@ const STATE_INICIAL = {
 }
 
 const CrearCuenta = () => {
-  const {valores, errores, submitForm, handleSubmit, handleChange} = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta)
+  const {valores, errores, submitForm, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta)
   const {nombre, email, password} = valores
 
   function crearCuenta() {
@@ -43,8 +43,11 @@ const CrearCuenta = () => {
                 name='nombre'
                 value={nombre}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.nombre && <Error>{errores.nombre}</Error>}
 
             <Campo>
               <label htmlFor="email">Email</label>
@@ -55,8 +58,11 @@ const CrearCuenta = () => {
                 name='email'
                 value={email}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.email && <Error>{errores.email}</Error>}
 
             <Campo>
               <label htmlFor="password">Password</label>
@@ -67,8 +73,11 @@ const CrearCuenta = () => {
                 name='password'
                 value={password}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.password && <Error>{errores.password}</Error>}
 
             <InputSubmit
               type="submit"
