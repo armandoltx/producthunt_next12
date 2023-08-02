671114900f0be45b01a2f7b6992279f94cc7ef48
Armando Ruiz <armando.ruiz@industrie.co>
2023-08-02 14:17:33 +1000

    616. Finalizando el hook para la validacion de formularios

--- hooks/useValidacion.js
+++ hooks/useValidacion.js
@@ -8,17 +8,39 @@ const useValidacion = (stateInicial, validar, fn) => {
   const [submitForm, guardarSubmitForm] = useState(false); // cuando el usuario le de a enviar cambia a true y el useEffects ejecuta el codigo
 
   useEffect(() => {
-    if (submitForm) {
+    if(submitForm) {
       const noErrores = Object.keys(errores).length === 0; // errores es un objeto y con Objects.keys verificamos si esta vacio
 
-      if (noErrores) {
+      if(noErrores) {
         fn(); // Fn = Función que se ejecuta en el componente
       }
       guardarSubmitForm(false);
     }
   }, []);
 
-  return (  );
+  // Función que se ejecuta conforme el usuario escribe algo
+  const handleChange = e => {
+    guardarValores({
+      ...valores,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // Función que se ejecuta cuando el usuario hace submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    const erroresValidacion = validar(valores);
+    guardarErrores(erroresValidacion);
+    guardarSubmitForm(true);
+  }
+
+  return {
+    valores,
+    errores,
+    submitForm,
+    handleSubmit,
+    handleChange
+  }
 }
 
 export default useValidacion;
