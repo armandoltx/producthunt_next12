cef8316ed90b7076e39657539444f257e13718a2
Armando Ruiz <armando.ruiz@industrie.co>
2023-08-04 09:21:11 +1000

    634. Consultando la base de datos de firebase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,38 @@
-import Head from 'next/head'
-import Layout from '../components/layouts/Layout'
+import React, {useEffect, useState, useContext} from "react"
+import Layout from "../components/layouts/Layout"
+import {FirebaseContext} from "../firebase"
+import {collection, getDocs} from 'firebase/firestore'
+
+const Home = () => {
+
+  const [productos, guardarProductos] = useState([])
+
+  const {firebase} = useContext(FirebaseContext)
+
+  useEffect(() => {
+    const obtenerProductos = async () => {
+      const querySnapshot = await getDocs(collection(firebase.db, "productos"));
+      const productos = querySnapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      });
+      guardarProductos(productos)
+
+    }
+    obtenerProductos()
+  }, [])
 
 
-export default function Home() {
+
   return (
     <div>
       <Layout>
         <h1>Inicio</h1>
       </Layout>
     </div>
-
   )
 }
+
+export default Home
