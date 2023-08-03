941492e3b3f59efefe29c46b139607d4b453b6ff
Armando Ruiz <armando.ruiz@industrie.co>
2023-08-03 14:16:22 +1000

    632. Creando la BD e insertando productos en Firestore

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {css} from '@emotion/react';
-import Router from 'next/router';
+import Router, {useRouter} from 'next/router';
 import Layout from '../components/layouts/Layout';
 import {Formulario, Campo, InputSubmit, Error} from '../components/ui/Formulario';
 
-import firebase from '../firebase';
+import {FirebaseContext} from '../firebase';
 
 // validaciones
 import useValidacion from '../hooks/useValidacion';
@@ -21,10 +21,37 @@ const STATE_INICIAL = {
 const NuevoProducto = () => {
 
   const [error, guardarError] = useState(false);
-  const {valores, errores, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearProducto, crearCuenta);
+
+  const {valores, errores, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearProducto, crearProducto);
+
   const {nombre, empresa, imagen, url, descripcion} = valores;
 
-  async function crearCuenta() {
+  // hook de routing para redireccionar
+  const router = useRouter();
+
+  // context con las operaciones crud de firebase
+  const {usuario, firebase} = useContext(FirebaseContext);
+
+  async function crearProducto() {
+
+    // si el usuario no esta autenticado llevar al login
+    if (!usuario) {
+      return router.push('/login');
+    }
+
+    // crear el objeto de nuevo producto
+    const producto = {
+      nombre,
+      empresa,
+      url,
+      descripcion,
+      votos: 0,
+      comentarios: [],
+      creado: Date.now()
+    }
+
+    // insertarlo en la base de datos
+    firebase.db.collection('productos').add(producto);
 
   }
 
@@ -76,20 +103,20 @@ const NuevoProducto = () => {
               </Campo>
 
               {errores.empresa && <Error>{errores.empresa}</Error>}
-{/*
-              <Campo>
-                <label htmlFor="imagen">Imagen</label>
-                <input
-                  type="file"
-                  id="imagen"
-                  name="imagen"
-                  value={imagen}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Campo> */}
 
-              {errores.imagen && <Error>{errores.imagen}</Error>}
+              {/* <Campo>
+                  <label htmlFor="imagen">Imagen</label>
+                  <input
+                      type="file"
+                      id="imagen"
+                      name="imagen"
+                      value={imagen}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                  />
+              </Campo>
+
+              {errores.imagen && <Error>{errores.imagen}</Error> } */}
 
               <Campo>
                 <label htmlFor="url">URL</label>
